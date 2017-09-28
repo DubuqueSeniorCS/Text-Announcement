2b9d651e0dbba5464117dc38efe0a6e35ebf79a5
Spellwizard <LiamJ.B.Johnston@gmail.com>
2017-09-27 19:50:54 -0500

    Update main.py

--- pi/main.py
+++ pi/main.py
@@ -3,21 +3,15 @@ from sense_hat import SenseHat#used to talk to sense hat
 import time #link
 
 from datetime import datetime,time#used to insure only runs on the specified times
-print("hi")
+print("Initiating Code")
 
-#link = "http://raw.githubusercontent.com/DubuqueSeniorCS/DubuqueCoders/master/Announcement%20System.txt" 
-#link = "http://seniormath.wikispaces.com/file/view/liam.txt/611094579/liam.txt"
 link ="https://pastebin.com/raw/gUUa97wg"
-
 f=requests.get(link)#reads the webpage
-
-#print(f.text) 
 sense=SenseHat()#makes sensehat
-
 def fullblock(color): 
   red= color#need to be 3 numbers (,,#)
 
-  rouge = [red,red,red,red,red,red,red,red,
+  color_array = [red,red,red,red,red,red,red,red,
          red,red,red,red,red,red,red,red,
          red,red,red,red,red,red,red,red,
          red,red,red,red,red,red,red,red,
@@ -25,27 +19,25 @@ def fullblock(color):
          red,red,red,red,red,red,red,red,
          red,red,red,red,red,red,red,red,
          red,red,red,red,red,red,red,red]
-  return rouge
+  return color_array
+#used to record the time between checking website
 count = 60 
 wait = 2 
 print("Iniating Code")
 
-now = datetime.now()
-now_time = now.time()
-while(now_time >= time(7,30) and now_time <= time(14,20)):#only runs in school hours
-  now = datetime.now()
-  now_time = now.time()
-  #print("Reading web\n") 
+def comm(ReCommq):
+  sense.show_message("",[0,0,0])
+
+while(true):#need to insert a sleep or standby function
   f=requests.get(link)#reads the webpage
 
   while "RED" in f.text:#checks for red alert issue
     f=requests.get(link)#reads the webpage
     sense.set_pixels(fullblock((255,0,0)))
     time.sleep(wait)
-    message = f.text.replace("RED"," ")
-    sense.show_message(message, text_colour = [255,0,0])
+    info = f.text.replace("RED"," ")
+    sense.show_message(info, text_colour = [255,0,0])
     
-  
   while "GREEN" in f.text:
     f=requests.get(link)#reads the webpage
     sense.set_pixels(fullblock((0,255,0)))
