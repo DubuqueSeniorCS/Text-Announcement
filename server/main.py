3dbc909ae080a8939f75e840db0b6ba15af3316e
Spellwizard <LiamJ.B.Johnston@gmail.com>
2017-09-28 00:14:06 +0000

    Update main.py

--- server/main.py
+++ server/main.py
@@ -1,1 +1,30 @@
-todo
+from flask import Flask, render_template, request, redirect
+app=Flask(__name__)
+
+message=""
+@app.route('/')
+def index():
+	return render_template('index.html')
+@app.route('/form', methods = ['POST'])
+def form():
+	global message
+	color = request.form['color']
+	message = request.form['message']
+	print("this is the data '" + color + message + "'")
+	message = color + " "+message
+	return redirect('/')
+
+
+@app.route('/announcement')
+def announcement():
+	return message
+	
+	
+
+
+
+
+
+
+if __name__=='__main__':
+	app.run(debug=True, host='0.0.0.0')
